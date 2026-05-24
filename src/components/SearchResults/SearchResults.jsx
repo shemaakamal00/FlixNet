dfd0026861aac0221ac1ee4e10d8dd5d127c5e49
Faisal Issaqi <faisalissaqi@gmail.com>
2026-05-25 01:11:23 +0200

    Implement search functionality with filters, pagination, and improved UI components

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,18 +1,28 @@
-import MovieCard from '../MovieCard/MovieCard'
-import styles from './SearchResults.module.css'
+import MovieCard from "../MovieCard/MovieCard";
+import { getDisplayTitle, getReleaseYear } from "../../utils/helpers";
+import styles from "./SearchResults.module.css";
 
 function SearchResults({ movies }) {
   if (movies.length === 0) {
-    return <p className={styles.message}>No movies found.</p>
+    return <p className={styles.message}>No movies found.</p>;
   }
 
   return (
     <div className={styles.grid}>
       {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
+        <article key={movie.id} className={styles.item}>
+          <MovieCard movie={movie} variant="search" />
+          <div className={styles.caption}>
+            <h3>{getDisplayTitle(movie)}</h3>
+            <p>
+              <span>{getReleaseYear(movie)}</span>
+              <span>{movie.vote_average ? movie.vote_average.toFixed(1) : "N/A"}</span>
+            </p>
+          </div>
+        </article>
       ))}
     </div>
-  )
+  );
 }
 
-export default SearchResults
+export default SearchResults;
